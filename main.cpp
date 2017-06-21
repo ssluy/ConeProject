fb26b4ee6be2a81264975bd2b8083e13f1c90fec
Sereysathia Luy <l.s.sathia2@gmail.com>
2017-06-21 13:23:56 +0700

    Add the Point and Cone Struct plus thier functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 #include <stdio.h>
 
+//----------------------------Point--------------------------------//
+/**
+ * This is the Structure for the Point
+ */
+struct Point{
+    double x;
+    double y;
+    double z;
+};
 
+/**
+ * Function to find the Distance between 2 Points
+ *
+ * @return
+ */
+double distance(struct Point1, struct Point2){
+
+}
+
+
+//----------------------------Cone---------------------------------//
+/**
+ * This is the Structure for the Cone
+ */
+struct Cone{
+    double radius;
+    double height;
+};
+
+/**
+ * This is the function to find the surface area of a cone
+ *
+ * @return
+ */
+double surfaceArea(double r, double h){
+
+}
+
+/**
+ * This is the function to find the Volume of a Cone
+ *
+ * @return
+ */
+double Volume(double r, double h){
+
+}
+
+//----------------------------Main---------------------------------//
+/**
+ *
+ * @return 0
+ */
 int main() {
 
     return 0;
